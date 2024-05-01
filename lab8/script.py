3d2ca63bcbb33d2714d22d086ae760222b4fec80
Maya Tietz <mayatietz@gmail.com>
2024-05-01 23:30:24 +0000

    lab 8

--- lab8/script.py
+++ lab8/script.py
@@ -1,12 +1,12 @@
-import requests
+import json
 
-url = "https://github.com/nmagee/ds2002-course/blob/main/labs/data/schacon.repos.json"
+fields = ['name','html_url','updated_at','visibility']
 
-response = requests.get(url)
-fields = ['name', 'html_url', 'updated_at', 'visibility']
-  
+with open('/workspace/json-practice/data/schacon.repos.json', 'r') as file:
+    data = json.load(file)
+
 with open('chacon.csv', 'w') as csv_file:
     csv_file.write(','.join(fields) + '\n')
-    for i in response.json():
+    for i in data[:5]:
         values = [str(i[field]) for field in fields]
         csv_file.write(','.join(values) + '\n')
